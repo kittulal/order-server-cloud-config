792578a612d1c845a407079b109652414a0d2145
Krishna Punna <punnakrishnaa@gmail.com>
2020-07-27 08:19:44 +0530

    first commit

---
+++ order-server-cloud-config/src/test/java/com/heliustech/orderservercloudconfig/OrderServerCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliustech.orderservercloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServerCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
